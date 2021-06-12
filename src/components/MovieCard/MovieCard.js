060c6ea48ffda3b7f2a9520359f303f328254ec3
victordoyle <victor-doyle@outlook.com>
2021-06-12 19:40:26 -0400

    added movie posters on homepage

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -3,12 +3,14 @@ import { Image, StyleSheet } from 'react-native';
 import { Container, Header, Content, Card, CardItem, Thumbnail, Text, Button, Icon, Left, Body, Right } from 'native-base';
 import ActionButtons from '../ActionButtons/ActionButtons';
 
-function MovieCard() {
+function MovieCard(props) {
+    let Image_Http_URL ={ uri: `https://www.themoviedb.org/t/p/w300_and_h450_bestv2/${props.image}`};
+
     return(
         <>                   
                     <Card style={styles.movieCardMain}>
                         <CardItem cardBody>
-                        <Image source={require('./movie1.jpg')} style={{height: 200, width: null, flex: 1}} style={styles.movieImageCard}/>
+                        <Image source={Image_Http_URL} style={{height: 200, width: null, flex: 1}} style={styles.movieImageCard}/>
                         </CardItem>
                         < >
                        <ActionButtons/>
