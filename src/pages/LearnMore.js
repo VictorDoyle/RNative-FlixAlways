bc02daa68ba45e33dbca3d6b11bf6b59d738b586
victordoyle <victor-doyle@outlook.com>
2021-06-25 16:49:13 -0400

    ternary setup for active button show

--- src/pages/LearnMore.js
+++ src/pages/LearnMore.js
@@ -1,37 +1,78 @@
-import React from "react";
+import React, { useReducer, useState } from "react";
 import { StyleSheet,SafeAreaView, View } from "react-native";
-import { Container, Header, Left, Body, Right, Button, Icon, Segment, Content, Text } from 'native-base';
+import { Container, Header, Left, Body, Right, Button, Icon, Segment, Content, Text, Title} from 'native-base';
+import { useHistory } from "react-router-native";
 
 function LearnMore() {
+    const history = useHistory();
+    function handleClick(path) {
+      history.push(path);
+    }
+
+    const [aboutUsActive, setAboutUsActive ] = useState(true);
+    const [aboutMoviesActive, setAboutMoviesActive ] = useState(false);
+    const [aboutHowActive, setAboutHowActive ] = useState(false);
+    const [buttonActiveColor, setButtonActiveColor] = useState()
+
+
     return(
         <>
         <SafeAreaView>
-  
             <Header hasSegment>
             <Left>
                 <Button transparent>
-                <Icon name="arrow-back" />
+                <Icon name="arrow-back" onPress={() => handleClick("")}/>
                 </Button>
             </Left>
             <Body>
                 <Segment>
-                <Button first><Text>Puppies</Text></Button>
-                <Button last active><Text>Cubs</Text></Button>
+  
+            <Button first onPress={() => {setAboutUsActive === false ? setAboutUsActive(true) : setAboutUsActive(false)}}>
+                    <Text>About Us</Text>
+                </Button>
+                <Button onPress={() => {setAboutMoviesActive === false ? setAboutMoviesActive(true) : setAboutMoviesActive(false)}}><Text>Movies</Text></Button>
+                <Button last onPress={() => {setAboutHowActive === false ? setAboutHowActive(true) : setAboutHowActive(false)}}><Text>How It Works</Text></Button>
                 </Segment>
             </Body>
-            <Right>
-                <Button transparent>
-                <Icon name="search" />
-                </Button>
-            </Right>
             </Header>
-            <Content padder>
-            <Text>Awesome segment</Text>
-            </Content>
-       
+       <Content>
+
+           {/* FIRST TAB */}
+           {aboutUsActive ? <> 
+           <Title> About FlixAlways</Title>
+           <Text> We Specialize In Bringing You The Best Movie Recommendations. Whether It's On Your Phone or Computer, We're Here To Help You Find What To Watch Next!</Text>
+           </> :
+           <>
+           </>}
+
+           {/* SECOND TAB */}
+           {aboutMoviesActive ? <> 
+           <Title> INTERACT WITH EACH MOVIE</Title>
+           <Text> Everytime you tell us which movies you've watched, liked, disliked or saved to view later, we'll make better movie recommendations for you. How? By never showing you the same movie recommendations twice!</Text>
+           </> :
+           <>
+           </>}
+
+           {/* THIRD TAB */}
+           {aboutHowActive ? <> 
+           <Title> How it Works</Title>
+           {/* use same as tutorial component here */}
+           </> :
+           <>
+           </>}
+       </Content>
         </SafeAreaView>
         </>
     )
 }
+
+
+const styles = StyleSheet.create({
+ /*    activeButtonStyling: {
+     backgroundColor: 
+    }, */
+   
+    
+  });
 
 export default LearnMore
